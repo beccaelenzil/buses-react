40bc090119190864214456b62b711f2164625345
becca <beccaelenzil@gmail.com>
2022-09-27 19:36:46 -0700

    google map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Wrapper } from "@googlemaps/react-wrapper";
+import React from "react";
+import Map from "./components/Map"
+import Marker from "./components/Marker"
+import schools from "./data/seattle_schools_list.json"
+
+
+const zoom = 11
+const center = {lat: 47.606, lng: -122.332}
+const markers = schools.map((school) => {
+  return {
+    lat: parseFloat(school["lat"]), 
+    lng: parseFloat(school["lng"])
+  }
+})
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ display: "flex", height: "100vh", width: "100vw" }}>
+      <Wrapper apiKey={"AIzaSyD4XxIrCSh0r6B6VKGLwX8EF8v1KfRFPBI"}>
+        <Map
+          center={center}
+          zoom={zoom}
+          style={{ flexGrow: "1", height: "100%" }}
         >
-          Learn React
-        </a>
-      </header>
+          {schools.map((school) => {
+            return <Marker position={{
+              lat: parseFloat(school["lat"]), 
+              lng: parseFloat(school["lng"])
+            }} />;
+          })}
+        </Map>
+      </Wrapper>
     </div>
   );
 }
 
 export default App;
+
